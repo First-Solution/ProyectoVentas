c53cb70977796e27e1ef8159f375a1963fd22262
Camilo Serrato <dcserratot@correo.udistrital.edu.co>
2021-09-21 14:39:31 -0500

    Adaptacion al proyecto

--- src/App.js
+++ src/App.js
@@ -3,22 +3,27 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <body>
+      <header>
+          <ul class = "navbar">
+              <li>
+                  <img src="./media/logo.png" alt="imagen" class="logo" />  
+              </li>
+              <li>
+                  <div class="buscar">
+                      <input placeholder="  Buscar Producto" />
+                    </div>
+                    <i class="fas fa-search botonGenerico iconoBusqueda"></i>
+              </li>
+          </ul>
       </header>
-    </div>
+      <main>
+
+      </main>
+      <footer>
+
+    </footer>
+    </body>
   );
 }
 
