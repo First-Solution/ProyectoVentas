f519d07edc6da29db69cd7dbb65aa2f22c69fbf9
Camilo Serrato <dcserratot@correo.udistrital.edu.co>
2021-09-22 13:03:01 -0500

    testing

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className = "App" >
+        <body>
+        <header >
+        </header>
+              <main>
+                <h1 className = "">
+                </h1>
+              </main>
+                <footer>
+                </footer>
+          </body>
+      </div>
+    
+  );
+}
+export default App;
