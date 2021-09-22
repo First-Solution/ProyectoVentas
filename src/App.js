1556c47079e9bba51d4670f669f5d5379111b3ca
Camilo Serrato <dcserratot@correo.udistrital.edu.co>
2021-09-21 20:38:31 -0500

    Agregar tailwind

--- src/App.js
+++ src/App.js
@@ -5,27 +5,32 @@ import './App.css';
 
 function App() {
   return (
+    <div className = "App" >
     <body>
-      <header>
-          <ul class = "navbar">
-              <li>
-                  <img src={logo}alt="imagen" class="logo" />  
-              </li>
-              <li>
-                  <div class="buscar">
-                      <input placeholder="  Buscar Producto" />
-                    </div>
-                    <i class="fas fa-search botonGenerico iconoBusqueda"></i>
-              </li>
-          </ul>
-      </header>
-      <main>
+          <header>
+              <ul class = "navbar">
+                  <li>
+                      <img src={logo}alt="imagen" class="logo" />  
+                  </li>
+                  <li>
+                      <div class="buscar">
+                          <input placeholder="  Buscar Producto" />
+                        </div>
+                        <i class="fas fa-search botonGenerico iconoBusqueda"></i>
+                  </li>
+              </ul>
+          </header>
+          <main>
+          <h1 className = "text-3xl mt-20 font-bold text-red-500">
+            Pagina de ventas
+          </h1>
+          </main>
+          <footer>
 
-      </main>
-      <footer>
-
-    </footer>
+        </footer>
     </body>
+    </div>
+    
   );
 }
 
