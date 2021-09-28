74083e7f47b653e44d53eb1b6211b70d5df8bb97
camilo <dcserratot@correo.udistrital.edu.co>
2021-09-28 16:14:06 -0500

    Complementacion de rutas

--- src/pages/Admin/registrarP.jsx
+++ src/pages/Admin/registrarP.jsx
@@ -1,5 +1,13 @@
 import { Link } from "react-router-dom";
-function RegistroP() {
+import React , {useEffect,useState} from 'react';
+import userEvent from "@testing-library/user-event";
+const RegistroP = () => {
+  const [nombreProducto,setNombreProducto]=useState("");
+  useEffect(()=>{
+  },[nombreProducto]);
+  const EnviarAlback = () =>{
+    console.log("El nombre del producto es",nombreProducto);
+   };
     return (
       <div className=" mx-auto max-w-6xl bg-white py-20 px-12 lg:px-24 shadow-xl mb-24">
         <h1 className = "text-gray-600  mx-auto text-center text-3xl py-3 font-semibold text-opacity-75">Registro Productos</h1>
@@ -10,7 +18,10 @@ function RegistroP() {
               <label class="uppercase tracking-wide text-black text-xs font-bold mb-2" for="company">
                 Nombre De Producto
               </label>
-              <input className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3" id="company" type="text" placeholder="Product"/>
+              <input onChange={ (e) =>{
+                setNombreProducto(e.target.value);
+              }}
+              value={nombreProducto}className="w-full bg-gray-200 text-black border border-gray-200 rounded py-3 px-4 mb-3" id="company" type="text" placeholder="Product"/>
 
             </div>
             <div className="md:w-1/2 px-3">
@@ -38,17 +49,23 @@ function RegistroP() {
           </div>
           <div className="-mx-3 md:flex mt-2">
           <div className="min-w-full md:flex md:flex-1 lg:w-0 py-2">
-              <Link to='/Vendedor/RegistroVentas'>
-              <button  className="sm:auto mx-auto ml-8 whitespace-nowrap px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+              
+              <button type="button" onClick= {EnviarAlback}className="sm:auto mx-auto ml-8 whitespace-nowrap px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
                 Registrar
               </button>
-             </Link>
+            
        </div>
           </div>
         </div>
       </form>
+   
     </div>
     );
   }
 
   export default RegistroP;
+  /*<Link to='/Admin/RegistroProductos'>
+              <button type="button" onClick= {EnviarAlback}className="sm:auto mx-auto ml-8 whitespace-nowrap px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+                Registrar
+              </button>
+             </Link>*/
