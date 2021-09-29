e38d82fefbe73e385798c448a9f09590ff909f3c
Camilo Serrato <dcserratot@correo.udistrital.edu.co>
2021-09-29 00:36:21 -0500

    Formularios de venta usuarios y producto terminados, formulario de producto con render de react

--- src/pages/Admin/registrarV.jsx
+++ src/pages/Admin/registrarV.jsx
@@ -70,11 +70,16 @@ function RegistroV() {
           </div>
           <div className="-mx-3 md:flex mt-2">
           <div className="min-w-full md:flex md:flex-1 lg:w-0 py-2">
-              <Link to='/Vendedor/RegistroVentas'>
+            
               <button  className="sm:auto mx-auto ml-8 whitespace-nowrap px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
                 Registrar
               </button>
-             </Link>
+             
+             <Link to='/Admin/Ventas'>
+              <button type="button" className="sm:auto mx-auto ml-8 whitespace-nowrap px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+                Ventas
+              </button>
+              </Link>
        </div>
           </div>
         </div>
