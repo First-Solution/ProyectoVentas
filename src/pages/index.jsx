b81c988a8acec3d66a9e19260625e899ae5a454c
Camilo Serrato <dcserratot@correo.udistrital.edu.co>
2021-10-03 21:08:27 -0500

    Carrusel de imagenes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,29 +4,29 @@ import ropa3 from "media/ropa3.jpg";
 function Index() {
     return (
      
-      <div id="carouselExampleCaptions" className=" flex flex-center justify-center  carousel slide p-3" data-bs-ride="carousel">
-      <div className="carousel-indicators flex-center">
+      <div id="carouselExampleCaptions" className="carousel slide p-3" data-bs-ride="carousel">
+      <div className="carousel-indicators ">
         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
       </div>
       <div className="carousel-inner">
         <div className="carousel-item active">
-          <img src={ropa2} className=" rounded-md " alt="..."/>
+          <img height="100" src={ropa2} className="rounded-3xl " alt="..."/>
           <div class="carousel-caption d-none d-md-block">
             <h5>First slide label</h5>
             <p>Some representative placeholder content for the first slide.</p>
           </div>
         </div>
         <div className="carousel-item">
-          <img src={ropa1} className=" rounded-md " alt="..."/>
+          <img src={ropa1} className=" rounded-3xl " alt="..."/>
           <div className="carousel-caption d-none d-md-block">
             <h5>Second slide label</h5>
             <p>Some representative placeholder content for the second slide.</p>
           </div>
         </div>
         <div className="carousel-item">
-          <img src={ropa3} className=" rounded-md " alt="..."/>
+          <img src={ropa3} className=" rounded-3xl " alt="..."/>
           <div className="carousel-caption d-none d-md-block">
             <h5>Third slide label</h5>
             <p>Some representative placeholder content for the third slide.</p>
