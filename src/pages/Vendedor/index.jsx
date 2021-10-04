78b70aab06bc8afb42d89a005414c5d883f0d2a6
Camilo Serrato <dcserratot@correo.udistrital.edu.co>
2021-10-03 21:09:19 -0500

    Carrusel de imagenes a todos los index

--- src/pages/Vendedor/index.jsx
+++ src/pages/Vendedor/index.jsx
@@ -1,11 +1,48 @@
-
+import ropa1 from "media/ropa1.jpg";
+import ropa2 from "media/ropa2.jpg";
+import ropa3 from "media/ropa3.jpg";
 function Index() {
     return (
-      <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-      <div className="px-4 py-6 sm:px-0">
-      <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
+     
+      <div id="carouselExampleCaptions" className="carousel slide p-3" data-bs-ride="carousel">
+      <div className="carousel-indicators ">
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
       </div>
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img height="100" src={ropa2} className="rounded-3xl " alt="..."/>
+          <div class="carousel-caption d-none d-md-block">
+            <h5>First slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img src={ropa1} className=" rounded-3xl " alt="..."/>
+          <div className="carousel-caption d-none d-md-block">
+            <h5>Second slide label</h5>
+            <p>Some representative placeholder content for the second slide.</p>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img src={ropa3} className=" rounded-3xl " alt="..."/>
+          <div className="carousel-caption d-none d-md-block">
+            <h5>Third slide label</h5>
+            <p>Some representative placeholder content for the third slide.</p>
+          </div>
+        </div>
       </div>
+      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
     );
   }
+
   export default Index;
