575a635d17b6fafce0271bc6a3b5f239adb382e0
Duvan <duvanga@hotmail.es>
2021-10-10 19:44:15 -0500

    agregar busqueda en page ventas vendedor y crud

--- src/pages/Vendedor/registrarV.jsx
+++ src/pages/Vendedor/registrarV.jsx
@@ -1,11 +1,15 @@
 import React, {useEffect, useState, useRef} from "react";
-import im from 'media/apreton.svg';
+/*import im from 'media/apreton.svg';*/
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import axios from 'axios';
+import { nanoid } from 'nanoid';
+import { obtenerVentas } from 'utils/api';
+import {Dialog, Tooltip} from '@material-ui/core'
 
 
-const ventasBackend = [
+
+/*const ventasBackend = [
   {
     idVenta: '01',
     nombreCl: 'Camilo',
@@ -39,19 +43,29 @@ const ventasBackend = [
     responsable: 'Andrea',
     producto: 'camisa'
   }
-];
+];*/
 
 
 const RegistroV = () => {
   const [mostrarTabla, setMostrarTabla] = useState(true)
   const [ventas, setVentas] = useState([])
   const [textoBoton, setTextoBoton] = useState("Registrar nueva venta")
+  const [ejecutarConsulta, setEjecutarConsulta] = useState(true)
 
-  useEffect(() =>{
-    // obtener lista de ventas desde el back
-    setVentas(ventasBackend)
+  useEffect(() => {
+    console.log('consulta', ejecutarConsulta);
+    if (ejecutarConsulta) {
+      obtenerVentas(setVentas, setEjecutarConsulta);
+    }
+  }, [ejecutarConsulta]);
 
-  },[])
+
+  useEffect(() => {
+    //obtener lista de ventas desde el backend
+    if (mostrarTabla) {
+      setEjecutarConsulta(true);
+    }
+  }, [mostrarTabla]);
 
 
 
@@ -67,7 +81,7 @@ const RegistroV = () => {
   return(
     <div>
       <h2 className="text-3xl font-extrabold text-gray-700">Pagina de administración de ventas</h2>
-      {mostrarTabla ? (<TablaVentas listaVentas = {ventas} />) :( <FormularioCrecionVentas 
+      {mostrarTabla ? (<TablaVentas listaVentas = {ventas} setEjecutarConsulta={setEjecutarConsulta} />) :( <FormularioCrecionVentas 
       setMostrarTabla ={setMostrarTabla} 
       listaVentas={ventas}
       setVentas={setVentas}/>)}
@@ -93,112 +107,317 @@ const RegistroV = () => {
   )
 }
 
-const TablaVentas = ({ listaVentas }) => {
-  useEffect(()=>{
-    console.log("listado de ventas", listaVentas)
-  },[listaVentas])
+const FilaVenta= ({ venta, setEjecutarConsulta }) => {
+
+  const [edit, setEdit] = useState(false)
+  const [openDialog, setOpenDialog] = useState(false);
+  const [infoNuevaVenta, setInfoNuevaVenta] = useState({
+    idVenta: venta.idVenta,
+    nombreCl: venta.nombreCl,
+    Estado: venta.Estado,
+    correo: venta.correo,
+    valor: venta.valor,
+    fechaI: venta.fechaI,
+    fechaF: venta.fechaF,
+    responsable: venta.responsable,
+    producto: venta.producto
+  });
+
+  const actualizarVenta = async () => {
+    //enviar la info al backend
+    const options = {
+      method: 'PATCH',
+      url: `http://localhost:5000/ventas/${venta._id}/`,
+      headers: { 'Content-Type': 'application/json' },
+      data: { ...infoNuevaVenta},
+    };
+
+    await axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        toast.success('Venta modificada con éxito');
+        setEdit(false);
+        setEjecutarConsulta(true);
+      })
+      .catch(function (error) {
+        toast.error('Error al modificar la venta');
+        console.error(error);
+      });
+  };
+
+  const eliminarVenta = async () => {
+    const options = {
+      method: 'DELETE',
+      url: `http://localhost:5000/ventas/${venta._id}/`,
+      headers: { 'Content-Type': 'application/json' },
+      data: { id: venta._id },
+    };
+
+    await axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        toast.success('venta eliminada con éxito');
+        setEjecutarConsulta(true);
+      })
+      .catch(function (error) {
+        console.error(error);
+        toast.error('Error al eliminar la venta');
+      });
+    setOpenDialog(false);
+  };
+
+  
 
   return(
-   <table className="min-w-full divide-y divide-gray-200">
-
-     <thead className="bg-gray-50">
-      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-        Id venta
-      </th>
-
-      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-        Cliente
-      </th>
-
-      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-        Valor
-      </th>
-
-      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-        Estado
-      </th>
-
-      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-        Fecha inicial
-      </th>
-
-      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-        Fecha final
-      </th>
-
-      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-        Responsable
-      </th>
-
-      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-        Producto
-      </th>
-
-      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-        Editar/Eliminar
-      </th>
-
-     </thead>
-
-     <tbody>
-       {listaVentas.map((venta)=>{
-         return(
-          <tr>
-          <td className="px-6 py-4 whitespace-nowrap">
-           <p className="text-sm text-gray-500">{venta.idVenta}</p>
+    <tr>
+      {edit ? (
+        <>
+          <td>
+            <input 
+              className = 'bg-gray-50 border border-gray-600 p-2 rounded-lg m-2' 
+              type='text' 
+              value={infoNuevaVenta.idVenta}
+              onChange={(e) => setInfoNuevaVenta({ ...infoNuevaVenta, name: e.target.value })}
+            />
           </td>
- 
-          <td className="px-6 py-4 whitespace-nowrap">
-            <div className="flex items-center">
-             <div className="flex-shrink-0 h-10 w-10">
-               <img className="h-10 w-10 rounded-full" src={im} alt=""/>
-             </div>
- 
-             <div className="ml-4">
-               <div className="text-sm font-medium text-gray-900">
-                 {venta.nombreCl}
-               </div>
-               <div className="text-sm text-gray-500">
-                 {venta.correo}
-               </div>
-             </div>
- 
-           </div>
-            
- 
+          <td>
+            <input 
+              className = 'bg-gray-50 border border-gray-600 p-2 rounded-lg m-2' 
+              type='text' 
+              value={infoNuevaVenta.nombreCl}
+              onChange={(e) => setInfoNuevaVenta({ ...infoNuevaVenta, nombreCl: e.target.value })}
+            />
           </td>
- 
-          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-            {venta.valor}
- 
+          <td>
+          <select 
+            className = 'bg-gray-50 border border-gray-600 p-2 rounded-lg m-2'
+            name='Estado'
+            value={infoNuevaVenta.Estado}
+            onChange={(e) => setInfoNuevaVenta({ ...infoNuevaVenta, Estado: e.target.value })}
+            >
+
+           <option disabled value={0}>Seleccione una opción</option>
+           <option >Embalaje</option>
+           <option >Entregado</option>
+         </select>
           </td>
- 
- 
-          <td className="px-6 py-4 whitespace-nowrap">
-            {venta.Estado}
- 
-           
-           
+          <td>
+            <input 
+              className = 'bg-gray-50 border border-gray-600 p-2 rounded-lg m-2' 
+              type='text'
+              value={infoNuevaVenta.correo}
+              onChange={(e) => setInfoNuevaVenta({ ...infoNuevaVenta, correo: e.target.value })} 
+            />
           </td>
- 
-          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{venta.fechaI}</td>
-          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{venta.fechaF}</td>
-          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{venta.responsable}</td>
-          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{venta.producto}</td>
-          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">editar/eliminar</td>
-        </tr>
-         )
+          <td>
+            <input 
+              className = 'bg-gray-50 border border-gray-600 p-2 rounded-lg m-2' 
+              type='text' 
+              value={infoNuevaVenta.valor}
+              onChange={(e) => setInfoNuevaVenta({ ...infoNuevaVenta, valor: e.target.value })} 
+            />
+          </td>
+          <td>
+            <input 
+              className = 'bg-gray-50 border border-gray-600 p-2 rounded-lg m-2' 
+              type='text'
+              value={infoNuevaVenta.fechaI}
+              onChange={(e) => setInfoNuevaVenta({ ...infoNuevaVenta, fechaI: e.target.value })}  
+            />
+          </td>
+          <td>
+            <input 
+              className = 'bg-gray-50 border border-gray-600 p-2 rounded-lg m-2' 
+              type='text' 
+              value={infoNuevaVenta.fechaF}
+              onChange={(e) => setInfoNuevaVenta({ ...infoNuevaVenta, fechaF: e.target.value })} 
+            />
+          </td>
+          <td>
+            <input 
+              className = 'bg-gray-50 border border-gray-600 p-2 rounded-lg m-2' 
+              type='text' 
+              value={infoNuevaVenta.responsable}
+              onChange={(e) => setInfoNuevaVenta({ ...infoNuevaVenta, responsable: e.target.value})} 
+            />
+          </td>
+          <td>
+            <input 
+              className = 'bg-gray-50 border border-gray-600 p-2 rounded-lg m-2' 
+              type='text' 
+              value={infoNuevaVenta.producto}
+              onChange={(e) => setInfoNuevaVenta({ ...infoNuevaVenta, producto: e.target.value})} 
+            />
+          </td>
+        </>
+      ) : (
+        <>
+          <td>{venta.idVenta}</td>
+          <td>{venta.nombreCl}</td>
+          <td>{venta.Estado}</td>
+          <td>{venta.correo}</td>
+          <td>{venta.valor}</td>
+          <td>{venta.fechaI}</td>
+          <td>{venta.fechaF}</td>
+          <td>{venta.responsable}</td>
+          <td>{venta.producto}</td>
+        </>
+      )}
+      <td>
+        <div className='flex w-full justify-around'>
+          {edit ? (
+            <>
+              <Tooltip title='Confirmar Edición' arrow>
+                <i
+                  onClick={() => actualizarVenta()}
+                  className='fas fa-check text-green-700 hover:text-green-500'
+                />
+              </Tooltip>
+              <Tooltip title='Cancelar edición' arrow>
+                <i
+                  onClick={() => setEdit(!edit)}
+                  className='fas fa-ban text-yellow-700 hover:text-yellow-500'
+                />
+              </Tooltip>
+            </>
+          ) : (
+            <>
+              <Tooltip title='Editar Vehículo' arrow>
+                <i
+                  onClick={() => setEdit(!edit)}
+                  className='fas fa-pencil-alt text-yellow-700 hover:text-yellow-500'
+                />
+              </Tooltip>
+              <Tooltip title='Eliminar Vehículo' arrow>
+                <i
+                  onClick={() => setOpenDialog(true)}
+                  className='fas fa-trash text-red-700 hover:text-red-500'
+                />
+              </Tooltip>
+            </>
+          )}
+        </div>
+        <Dialog open={openDialog}>
+          <div className='p-8 flex flex-col'>
+            <h1 className='text-gray-900 text-2xl font-bold'>
+              ¿Está seguro de querer eliminar la venta?
+            </h1>
+            <div className='flex w-full items-center justify-center my-4'>
+              <button
+                onClick={() => eliminarVenta()}
+                className='mx-2 px-4 py-2 bg-green-500 text-white hover:bg-green-700 rounded-md shadow-md'
+              >
+                Sí
+              </button>
+              <button
+                onClick={() => setOpenDialog(false)}
+                className='mx-2 px-4 py-2 bg-red-500 text-white hover:bg-red-700 rounded-md shadow-md'
+              >
+                No
+              </button>
+            </div>
+          </div>
+        </Dialog>
+      </td>
+    </tr>
 
+    
 
-       })}
-
-     </tbody>
-
-     
-   </table>
   )
 }
 
+
+const TablaVentas = ({ listaVentas, setEjecutarConsulta }) => {
+  const [busqueda, setBusqueda] = useState('');
+  const [ventasFiltradas, setVentasFiltradas] = useState(listaVentas);
+
+  useEffect(() => {
+    setVentasFiltradas(
+      listaVentas.filter((elemento) => {
+        return JSON.stringify(elemento).toLowerCase().includes(busqueda.toLowerCase());
+      })
+    );
+  }, [busqueda, listaVentas]);
+
+
+  /*useEffect(()=>{
+    console.log("listado de ventas", listaVentas)
+  },[listaVentas])*/
+
+  return(
+    <div>
+        <input 
+        type="text" 
+        placeholder="Buscar" 
+        className = 'bg-gray-50 border border-gray-600 p-2 rounded-lg m-2'
+        value={busqueda}
+        onChange={(e) => setBusqueda(e.target.value)}
+        />
+
+        <table className="min-w-full divide-y divide-gray-200">
+
+          <thead className="bg-gray-50">
+          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Id venta
+          </th>
+
+          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Cliente
+          </th>
+
+          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Valor
+          </th>
+
+          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Estado
+          </th>
+
+          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Fecha inicial
+          </th>
+
+          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Fecha final
+          </th>
+
+          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Responsable
+          </th>
+
+          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Producto
+          </th>
+
+          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            Editar/Eliminar
+          </th>
+
+          </thead>
+
+          <tbody>
+            {ventasFiltradas.map((venta)=>{
+              return(
+                <FilaVenta key={nanoid()} venta={venta} setEjecutarConsulta = {setEjecutarConsulta} />
+
+              )
+
+            })}
+
+          </tbody>
+
+
+          </table>
+
+
+    </div>
+
+
+ 
+  )
+}
 
 
 const FormularioCrecionVentas = ({setMostrarTabla, listaVentas, setVentas  }) => {
@@ -238,17 +457,27 @@ const FormularioCrecionVentas = ({setMostrarTabla, listaVentas, setVentas  }) =>
     
     e.preventDefault();
     const fd = new FormData(form.current);
-
-    const nuevoVehiculo = {};
+    const nuevaVenta = {};
     fd.forEach((value, key) => {
-      nuevoVehiculo[key] = value;
+      nuevaVenta[key] = value;
+      console.log(nuevaVenta[key], key)
     });
 
     const options = {
       method: 'POST',
-      url: 'https://vast-waters-45728.herokuapp.com/venta/create',
+      url: 'http://localhost:5000/ventas/',
       headers: { 'Content-Type': 'application/json' },
-      data: { name: nuevoVehiculo.name, brand: nuevoVehiculo.brand, model: nuevoVehiculo.model },
+      //data la informacion que pide del backend
+      data: {  
+        idVenta: nuevaVenta.idVenta,
+        nombreCl: nuevaVenta.nombreCl,
+        Estado: nuevaVenta.Estado,
+        correo: nuevaVenta.correo,
+        valor: nuevaVenta.valor,
+        fechaI: nuevaVenta.fechaI,
+        fechaF: nuevaVenta.fechaF,
+        responsable: nuevaVenta.responsable,
+        producto: nuevaVenta.producto },
     };
 
     await axios
@@ -295,7 +524,7 @@ const FormularioCrecionVentas = ({setMostrarTabla, listaVentas, setVentas  }) =>
          Nombre Cliente
          <input className = 'bg-gray-50 border border-gray-600 p-2 rounded-lg m-2' 
          type='text' 
-         name = 'nombre cliente'
+         name = 'nombreCl'
          />
        </label>
 
@@ -375,3 +604,5 @@ const FormularioCrecionVentas = ({setMostrarTabla, listaVentas, setVentas  }) =>
 
 
   export default RegistroV;
+
+
