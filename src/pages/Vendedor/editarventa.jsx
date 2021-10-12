d52a80e5d37c8412aa21e7358bdf692c8a05f1da
Mauro <esmauricio@gmail.com>
2021-10-12 00:56:33 -0500

    Vinclular pagina de ventas al componente tableventas

--- src/pages/Vendedor/editarventa.jsx
+++ src/pages/Vendedor/editarventa.jsx
@@ -150,10 +150,10 @@ const EditV = () =>
           <div className="min-w-full md:flex md:flex-1 lg:w-0 py-2">
               
               <button  type = "button" onClick = {EnviarAlback} className="sm:auto mx-auto ml-8 whitespace-nowrap px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
-                Editar
+                editar
               </button>
              
-             <Link to='/Vendedor/Ventas'>
+             <Link to='/Admin/Ventas'>
               <button type="button" className="sm:auto mx-auto ml-8 whitespace-nowrap px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
                 Ventas
               </button>
--- src/pages/Vendedor/ventas.jsx
+++ src/pages/Vendedor/ventas.jsx
@@ -1,11 +1,10 @@
-import Tabla from 'components/tableventasV';
+import Tabla from 'components/tableventas';
 function Ventas() {
-  
     return (
-      <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-      <div class="px-4 py-6 sm:px-0">
+      <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+      <div className="px-4 py-6 sm:px-0">
       
-        <Tabla/>
+      <Tabla/>
       
       </div>
       </div>
